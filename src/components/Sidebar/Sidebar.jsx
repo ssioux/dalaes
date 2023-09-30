9c061d9027f409555f3cc4964694deeab14bd063
David Lazaro <dlazaro7@gmail.com>
2023-09-30 19:30:29 +0200

    sidebar active icons done

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,40 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './sidebar.scss'
 import LogoSsioux from '../../assets/images/logo-ssioux.png'
 import LogoSsiouxSubtitle from '../../assets/images/logo-ssioux-sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
-  return <div className='nav-bar'>
-    <Link className='logo' to='/'>
-        <img src={LogoSsioux} alt='logo'/>
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoSsioux} alt="logo" />
         <img className="sub-logo" src={LogoSsiouxSubtitle} alt="Ssioux" />
-    </Link>
-  </div>
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+    </div>
+  )
 }
 
 export default Sidebar
