e4980e70fd611572b5a23f866a8e384fb6d4db19
David Lazaro <dlazaro7@gmail.com>
2023-10-01 03:00:24 +0200

    Home component updated with individual letters in span

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,59 @@
 import './home.scss'
 import LogoTitle from '../../assets/images/logo-ssioux.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['s', 'i', 'o', 'u', 'x']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br /> I am 
-        <img src={LogoTitle} alt="developer" />
-        sioux
-        <br />
-        web developer
-          </h1>
-          <h2>Frontend Developer / Junior </h2>
-          <Link to='/contact' className='flat-button'>CONTACT ME</Link>
-       
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+
+          <img src={LogoTitle} alt="developer" />
+
+          {/* Animation for 'sioux' */}
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            index={15}
+          />
+          <br />
+          {/* Animation for 'web developer.' */}
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            index={22}
+          />
+        </h1>
+        <h2>Frontend Developer / Junior </h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
       </div>
     </div>
   )
