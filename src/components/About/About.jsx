6001885bf98d695c908a93056504747fb5087dc5
David Lazaro <dlazaro7@gmail.com>
2023-10-02 16:38:15 +0200

    About component updated + languages logo uploaded

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import './about.scss'
 
 const About = () => {
+
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(()=> {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
   return (
     <>
       <div className="container about-page">
         <div className="text-zone">
           <h1>
             <AnimatedLetters
+              letterClass={letterClass}
               strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
               idx={15}
             />
