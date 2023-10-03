3c83b66ecf278d7cd15378351af1099b65399437
David Lazaro <dlazaro7@gmail.com>
2023-10-03 13:36:42 +0200

    Contact route and component added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -32,12 +32,12 @@ const About = () => {
             />
           </h1>
           <p>
-            I'm a very ambitious front-end developer looking for a role in an
+            I am a very ambitious front-end developer looking for a role in an
             established IT company with the opportunity to work with the latest
             technologies on challenging and diverse projects.
           </p>
           <p>
-            I'm quiet confident, naturally curious, and perpetually working on
+            I am quite confident, naturally curious, and perpetually working on
             improving my chops one design problem at a time.
           </p>
           <p>
