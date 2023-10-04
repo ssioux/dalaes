c46818a1d9ab40b9fd383e4626b2b3e9b606c2c9
David Lazaro <dlazaro7@gmail.com>
2023-10-04 12:18:56 +0200

    contact form done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
   }, [])
   return (
     <>
-       <div className="container contact-page">
+      <div className="container contact-page">
         <div className="text-zone">
           <h1>
             <AnimatedLetters
@@ -24,9 +24,48 @@ const Contact = () => {
               idx={15}
             />
           </h1>
+          <p>
+            I am interested in companies that can give me the opportunity to
+            improve my skills. However, if you have any other requests or
+            questions, don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
       </div>
-      </div>
-
 
       <Loader type="pacman" />
     </>
