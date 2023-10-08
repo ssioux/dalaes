0f6edc12beecd871fabf0a2d9189856529f5854f
David Lazaro <dlazaro7@gmail.com>
2023-10-08 19:42:30 +0200

    Dasboard component updated

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,23 @@
+// Hooks
+import { useEffect, useState } from 'react'
+// Firebase Auth
+import { onAuthStateChanged, getAuth } from 'firebase/auth'
+// Components
+import Login from '../Login/Login'
+import Main from './Main'
+
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+  const [user, setUser] = useState(null)
+  const auth = getAuth()
+
+  useEffect(() => {
+    // 1-auth, 2-method
+    onAuthStateChanged(auth, (user) => {
+      user ? setUser(user) : setUser(null)
+    })
+  }, [])
+
+  return <div>{user ? <Main /> : <Login />}</div>
 }
 
 export default Dashboard
