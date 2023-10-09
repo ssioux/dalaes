9f4042d3430ac0f36ce62f7e04c1b6674add484b
David Lazaro <dlazaro7@gmail.com>
2023-10-09 20:53:11 +0200

    porfolio component updated

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,37 @@
+import Loader from 'react-loaders'
 import './portfolio.scss'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import { useEffect, useState } from 'react'
 
 const Portfolio = () => {
+
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const letterMouseMovement = () => {
+           setTimeout(()=> {
+       setLetterClass('text-animate-hover')
+     }, 3000)
+    }
+    return (
+     letterMouseMovement()
+        )
+   }, [])
   return (
-    <div>Portfolio</div>
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+
+          <AnimatedLetters 
+          letterClass={letterClass}
+          strArray={"Portfolio".split("")}
+            idx={15}
+          
+          />
+        </h1>
+      </div>
+      <Loader type="pacman"/>
+    </>
   )
 }
 
