96fa1d4fa903fc9f75ef301e88625a5ea86e40d8
David Lazaro <dlazaro7@gmail.com>
2023-10-28 12:06:53 +0200

    chess added with stupid ai xD

--- src/components/Chess/Game.jsx
+++ src/components/Chess/Game.jsx
@@ -3,9 +3,11 @@ import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 
+import {Chessboard} from 'react-chessboard'
+import {Chess} from 'chess.js'
+
 function Game() {
   const [letterClass, setLetterClass] = useState('text-animate')
-
   useEffect(() => {
     const letterMouseMovement = setTimeout(() => {
       setLetterClass('text-animate-hover')
@@ -15,6 +17,53 @@ function Game() {
       clearTimeout(letterMouseMovement)
     }
   })
+
+  const [game, setGame] = useState(new Chess());
+  console.log("first",game)
+  //Let's perform a function on the game state 
+   
+  function safeGameMutate(modify){
+    setGame((g)=>{
+      const update = {...g}
+      modify(update)
+      return update;
+    })
+  }
+  //Movement of computer
+  function makeRandomMove(){
+    const possibleMove = game.moves();
+  
+    //exit if the game is over 
+  
+    if(game.game_over() || game.in_draw() || possibleMove.length === 0) return;
+    //select random move
+  
+    const randomIndex = Math.floor(Math.random() * possibleMove.length);
+   //play random move 
+   safeGameMutate((game)=>{
+    game.move(possibleMove[randomIndex]);
+   })
+  }
+  
+  //Perform an action when a piece is droped by a user
+   
+  function onDrop(source,target){
+    let move = null;
+    safeGameMutate((game)=>{
+      move = game.move({
+        from:source,
+        to: target,
+        promotion:'q'
+      })
+  })
+   //illegal move 
+   if(move== null) return false
+   //valid move 
+   setTimeout(makeRandomMove, 200);
+   return true;
+  }
+
+
 
   return (
     <>
@@ -26,6 +75,13 @@ function Game() {
             idx={15}
           />
         </h1>
+
+        <div className="board">
+      <Chessboard 
+      position={game.fen()}
+      onPieceDrop ={onDrop}
+      />
+    </div>
       </div>
       <Loader type="pacman" />
     </>
