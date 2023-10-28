9b6eec51e3c150e547cfd7d2dcd9857fe4ff680a
David Lazaro <dlazaro7@gmail.com>
2023-10-28 02:07:24 +0200

    chess component updated

--- src/components/Chess/Chess.jsx
+++ src/components/Chess/Chess.jsx
@@ -1,8 +1,36 @@
 import './chess.scss'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 
 function Chess() {
+
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const letterMouseMovement = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    return () => {
+      clearTimeout(letterMouseMovement)
+    }
+  })
+
   return (
-    <div>Chess</div>
+    <>
+      <div className="container chess-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Chess'.split('')}
+            idx={15}
+          />
+        </h1>
+      
+      </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
