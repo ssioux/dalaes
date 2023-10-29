d02e08f5e585be3b42192bf2d0d2a962f4739b0f
David Lazaro <dlazaro7@gmail.com>
2023-10-29 22:07:28 +0100

    chess onDrop fixed

--- src/components/Chess/Game.jsx
+++ src/components/Chess/Game.jsx
@@ -1,23 +1,26 @@
 import './game.scss'
+
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 
-import {Chessboard} from 'react-chessboard'
-import {Chess} from 'chess.js'
-import { calculateBestMove, initGame } from "chess-ai";
+import { Chessboard } from 'react-chessboard'
+import { Chess } from 'chess.js'
+import { calculateBestMove, initGame } from 'chess-ai'
 
 function Game() {
+  console.log('chess', Chess)
+  console.log('board', Chessboard)
 
   const [letterClass, setLetterClass] = useState('text-animate')
-  
+  // new Game
+  const chess = new Chess()
+  console.log('chess', chess)
+  const [game, setGame] = useState(chess)
 
-
-  const [game, setGame] = useState(new Chess());
-
+  // TODO: when the game is over alert the user
   useEffect(() => {
-
-    initGame(game, 1);
+    initGame(game, 1)
     const letterMouseMovement = setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
@@ -27,51 +30,58 @@ function Game() {
     }
   })
 
-  //Let's perform a function on the game state 
-   
-  function safeGameMutate(modify){
-    setGame((g)=>{
-      const update = {...g}
+  //Let's perform a function on the game state
+
+  function safeGameMutate(modify) {
+    setGame((g) => {
+      const update = { ...g }
       modify(update)
-      return update;
+      return update
     })
   }
   //Movement of computer
-  function makeRandomMove(){
-    const possibleMove = game.moves();
-  
-    //exit if the game is over 
-  
-    if(game.game_over() || game.in_draw() || possibleMove.length === 0) return;
-    //select random move
-  
-    // const randomIndex = Math.floor(Math.random() * possibleMove.length);
-   
-   //play random move 
-   safeGameMutate((game)=>{
-    game.move(calculateBestMove());
-   })
+  function makeRandomMove() {
+    const possibleMove = game.moves()
+
+    //exit if the game is over
+
+    if (
+      game.game_over() ||
+      game.in_draw() ||
+      possibleMove.length === 0 ||
+      game.in_checkmate()
+    ) {
+      alert('Game is over')
+    }
+    if (game.in_check()) {
+      console.log('first')
+    }
+    //select ai move
+    const aiMove = calculateBestMove()
+
+    //play random move
+    safeGameMutate((game) => {
+      game.move(aiMove)
+    })
   }
-  
+
   //Perform an action when a piece is droped by a user
-   
-  function onDrop(source,target){
-    let move = null;
-    safeGameMutate((game)=>{
+
+  function onDrop(source, target) {
+    let move = null
+    safeGameMutate((game) => {
       move = game.move({
-        from:source,
+        from: source,
         to: target,
-        promotion:'q'
+        promotion: 'q',
       })
-  })
-   //illegal move 
-   if(move== null) return false
-   //valid move 
-   setTimeout(makeRandomMove, 200);
-   return true;
+    })
+    //illegal move
+    if (move == null) return false
+    //valid move
+    setTimeout(makeRandomMove, 200)
+    return true
   }
-
-
 
   return (
     <>
@@ -85,11 +95,14 @@ function Game() {
         </h1>
 
         <div className="board">
-      <Chessboard 
-      position={game.fen()}
-      onPieceDrop ={onDrop}
-      />
-    </div>
+          <div className="bordered">
+            <Chessboard
+              position={game.fen()}
+              onPieceDrop={onDrop}
+              boardWidth={800}
+            />
+          </div>
+        </div>
       </div>
       <Loader type="pacman" />
     </>
